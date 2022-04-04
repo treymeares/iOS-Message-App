49cbcf191427914ebe4023f19e0f576e2e736c4d
Trey Meares <trey.meares@me.com>
2022-04-04 13:24:55 -0400

    Added resources for code reuse

--- Controllers/Register_and_Login/LoginViewController.swift
+++ Controllers/Register_and_Login/LoginViewController.swift
@@ -8,14 +8,36 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "Login"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
+        
+        //Add subview
+        view.addSubview(imageView)
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.frame.size.width/3
+        imageView.frame = CGRect(x: (view.frame.size.width-size) / 2, y: 20, width: size, height: size)
+    }
+    
+    @objc private func didTapRegister() {
+        let rVC = RegisterViewController()
+        rVC.title = "Create New Account"
+        navigationController?.pushViewController(rVC, animated: true)
+    }
 
     /*
     // MARK: - Navigation
---
+++ Resources/Ext.swift
@@ -0,0 +1,36 @@
+//
+//  Ext.swift
+//  Sp22_Message_App
+//
+//  Created by Trey Meares on 4/4/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    public var bottom: CGFloat {
+        return self.frame.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+}
+
