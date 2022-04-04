7b32285d78dad2382be36ad5816f6fb982f19ad4
Trey Meares <trey.meares@me.com>
2022-04-04 12:58:42 -0400

    Added View Controller and ReOrganized

---
+++ Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sp22_Message_App
+//
+//  Created by Trey Meares on 4/4/22.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
