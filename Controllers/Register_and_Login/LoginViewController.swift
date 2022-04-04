1495736d3c51c14b330ad18360b04ba1fe357eb1
Trey Meares <trey.meares@me.com>
2022-04-04 13:41:36 -0400

    Added Register Items

--- Controllers/Register_and_Login/LoginViewController.swift
+++ Controllers/Register_and_Login/LoginViewController.swift
@@ -9,28 +9,51 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "logo")
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocorrectionType = .no
+        field.autocapitalizationType = .none
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 6
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Enter E-mail Address"
+        
+        return field
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "Login"
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
         
-        //Add subview
-        view.addSubview(imageView)
+        //Add Scroll View Subview
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
         // Do any additional setup after loading the view.
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let size = view.frame.size.width/3
-        imageView.frame = CGRect(x: (view.frame.size.width-size) / 2, y: 20, width: size, height: size)
+        scrollView.frame = view.bounds
+        let size = scrollView.width / 3
+        imageView.frame = CGRect(x: (scrollView.width-size) / 2, y: 20, width: size, height: size)
+        emailField.frame = CGRect(x: 30, y: imageView.bottom+10, width:scrollView.width - 60, height: 52)
     }
     
     @objc private func didTapRegister() {
